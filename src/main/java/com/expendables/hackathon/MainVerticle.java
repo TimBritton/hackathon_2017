cc70ac66a9baedc033f0c362b24e1e87fb06701c
TimBritton <ziberath@gmail.com>
2017-12-02 11:31:22 -0600

    Convert To Java

--- src/main/java/com/expendables/hackathon/MainVerticle.java
+++ src/main/java/com/expendables/hackathon/MainVerticle.java
@@ -1,5 +1,6 @@
 package com.expendables.hackathon;
 
+import com.expendables.hackathon.api.HttpJavaVerticle;
 import com.expendables.hackathon.api.HttpVerticle;
 import io.vertx.core.AbstractVerticle;
 
@@ -7,7 +8,7 @@ public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
-        vertx.deployVerticle(new HttpVerticle());
+        vertx.deployVerticle(new HttpJavaVerticle());
         vertx.deployVerticle("service:io.vertx.vertx-mongo-embedded-db");
         System.out.println("HTTP server started on port 8080");
     }
---
+++ src/main/java/com/expendables/hackathon/api/HttpJavaVerticle.java
@@ -0,0 +1,59 @@
+package com.expendables.hackathon.api;
+
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+
+public class HttpJavaVerticle extends AbstractVerticle{
+    /**
+     * If your verticle does a simple, synchronous start-up then override this method and put your start-up
+     * code in here.
+     *
+     * @throws Exception
+     */
+    @Override
+    public void start() throws Exception {
+
+        HttpServer server = vertx.createHttpServer();
+
+        Router router = Router.router(vertx);
+
+        router.get("/api/v1/status/road/:address").handler(
+            event -> {
+                String requestedRoad  = event.request().getParam("address");
+
+            //Okay at this point we need to find the path that represents the given road
+
+            //Then we check to see if we have any nodes near that path
+
+            //Then we check the status of all of those points and determine the likelihood that
+            //there is ice
+//            val retJson = JsonObject(
+//            "icy" to true,
+//            "certainty" to 80.0
+//            )
+
+        event.response().putHeader(HttpHeaderNames.CONTENT_TYPE.toString(), HttpHeaderValues.APPLICATION_JSON.toString()).end(new JsonObject().encodePrettily());
+
+        });
+
+        router.get("/api/v1/status/:sensorId").handler(
+            event ->{
+
+            String sensorId = event.request().getParam("sensorId");
+
+
+        });
+
+        router.route("/*").handler(StaticHandler.create());
+
+        server.requestHandler(it -> router.accept(it) ).listen(8080);
+        super.start();
+
+    }
+}
