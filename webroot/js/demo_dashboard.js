432678dae52f8519a3d78f514ecc7a59e683c0dd
Lazaro Sanchez <laza.sanchez@gmail.com>
2017-12-03 13:17:30 -0600

    Added webapp

--- webroot/js/demo_dashboard.js
+++ webroot/js/demo_dashboard.js
@@ -1,4 +1,18 @@
 $(function () {
+
+    var HttpClient = function () {
+        this.get = function (aUrl, aCallback) {
+            var anHttpRequest = new XMLHttpRequest();
+            anHttpRequest.responseType = 'json';
+            anHttpRequest.onreadystatechange = function () {
+                if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                    aCallback((anHttpRequest.response));
+            }
+            anHttpRequest.open("GET", aUrl, true);
+            anHttpRequest.send(null);
+        }
+    }
+
     var self = this;
     var updateIcons = function (map, sensors) {
         var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
@@ -13,18 +27,23 @@ $(function () {
 
         var features = [
             {
+                sensorId: "FRSTY:ID:ce22de3b-a771-4491-8dc2-9kka4sas",
                 position: new google.maps.LatLng(41.8963241, -87.65200379999999),
                 type: 'snowflake'
             }, {
+                sensorId: "FRSTY:ID:da22dy3b-h903-4561-8dc2-4e45d35",
                 position: new google.maps.LatLng(41.89631420000001, -87.6526083),
                 type: 'snowflake'
             }, {
+                sensorId: "FRSTY:ID:87h2de3b-c332-891s-8dc2-4e84d35",
                 position: new google.maps.LatLng(41.8946913, -87.65300230000003),
                 type: 'snowflake'
             }, {
+                sensorId: "FRSTY:ID:90ie3b-f771-9376-9l3d-42cad35",
                 position: new google.maps.LatLng(41.8911906, -87.64761729999998),
                 type: 'sunny'
             }, {
+                sensorId: "FRSTY:ID:9845sb-ju31-4923-093j-jnuhy3dui",
                 position: new google.maps.LatLng(41.8903461, -87.6340424),
                 type: 'sunny'
             }
@@ -44,11 +63,11 @@ $(function () {
         for (var i = 0; i < features.length; i++) {
             row = table.insertRow(i + 1);
             row.insertCell(0).innerHTML = "" + i;
-            row.insertCell(1).innerHTML = features[i].position.lat();
-            row.insertCell(2).innerHTML = features[i].position.lng();
-            row.insertCell(3).innerHTML = (features[i].type == 'sunny') ? "NO" : "YES";
+            row.insertCell(1).innerHTML = features[i].sensorId;
+            row.insertCell(2).innerHTML = features[i].position.lat();
+            row.insertCell(3).innerHTML = features[i].position.lng();
+            row.insertCell(4).innerHTML = (features[i].type == 'sunny') ? "NO" : "YES";
         }
-
         // Create markers.
         features.forEach(function (feature) {
             var marker = new google.maps.Marker({
@@ -57,28 +76,46 @@ $(function () {
                 map: map
             });
         });
+        // if (sensors !== undefined) {
+        //     for (var i = 0; i < sensors.length; i++) {
+        //         row = table.insertRow(i + 1);
+        //         row.insertCell(0).innerHTML = "" + i;
+        //         row.insertCell(1).innerHTML = sensors[i].sensorId;
+        //         row.insertCell(2).innerHTML = sensors[i].position.lat();
+        //         row.insertCell(3).innerHTML = sensors[i].position.lng();
+        //         row.insertCell(4).innerHTML = (sensors[i].type == 'sunny') ? "NO" : "YES";
+        //     }
+        //
+        //     sensors.forEach(function (feature) {
+        //         console.log(feature);
+        //         var marker = new google.maps.Marker({
+        //             position: feature.position,
+        //             icon: icons[feature.type].icon,
+        //             map: map
+        //         });
+        //     });
+        // }
     };
 
     var getSensors = function (params) {
-        console.log(params);
         var url = "http://69.25.149.102:8080/api/v1/status/point?lon=" +
             params[0] + "&lat=" + params[1] + "&rad=" +
             params[2];
-        console.log(url);
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", url, true);
-        xhttp.setRequestHeader("Content-type", "application/json");
-        xhttp.send(null);
-        console.log(xhttp.responseText);
+        var result = [];
+        var client = new HttpClient();
+        client.get(url, function (response) {
+            for (var i = 0; i < response.length; i++) {
+                result.push({
+                    sensorId: response[i].sensorId,
+                    position: new google.maps.LatLng(response[i].location.coordinates[1], response[i].location.coordinates[0]),
+                    type: 'snowflake',
+                    sensorState: response[i].sensorState
+                })
+            }
+            console.log(result);
+            return result;
+        });
     };
-
-    var getIcePath = function (params) {
-        // var xhttp = new XMLHttpRequest();
-        // xhttp.open("GET", "/api/v1/status/route", true);
-        // xhttp.setRequestHeader("Content-type", "application/json");
-        // xhttp.send(params);
-        // return JSON.parse(xhttp.responseText);
-    }
 
     radiusChange = function (radius) {
         document.getElementById(radius + 'value').innerHTML =
@@ -110,35 +147,32 @@ $(function () {
         var gWorldOptions = {zoom: 15, center: gWorldCords, mapTypeId: google.maps.MapTypeId.ROADMAP}
         var gWorld = new google.maps.Map(document.getElementById("google_world_map"), gWorldOptions);
         initMap(gWorld);
-        updateIcons(gWorld, []);
+        var data = getSensors([gWorld.getCenter().lng(), gWorld.getCenter().lat(),
+            document.getElementById('radius1').value * 100]);
+        updateIcons(gWorld, data);
         gWorld.addListener('dragend', function () {
-            getSensors([gWorld.getCenter().lng(), gWorld.getCenter().lat(),
-                document.getElementById('radius2').value * 100]);
-            updateIcons(gWorld, []);
-            console.log(JSON.stringify({
-                lon: gWorld.getCenter().lng(), lat: gWorld.getCenter().lat(),
-                rad: document.getElementById('radius2').value
-            }));
+            var data = getSensors([gWorld.getCenter().lng(), gWorld.getCenter().lat(),
+                document.getElementById('radius1').value * 100]);
+            updateIcons(gWorld, data);
         });
     }
 
-    if ($("#google_map_route").length > 0) {
-
-        var gWorldCords = new google.maps.LatLng(41.895562, -87.65097);
-        var gWorldOptions = {zoom: 15, center: gWorldCords, mapTypeId: google.maps.MapTypeId.ROADMAP};
-        var gWorld1 = new google.maps.Map(document.getElementById("google_map_route"), gWorldOptions);
-        initMap(gWorld1);
-        new AutocompleteDirectionsHandler(gWorld1);
-        updateIcons(gWorld1, []);
-        gWorld1.addListener('dragend', function () {
-            updateIcons(gWorld1, []);
-            // getIcePath(JSON.stringify({location: [gWorld1.getCenter().lng(), gWorld1.getCenter().lat()], maxDistance: document.getElementById('radius2').value}));
-            console.log(JSON.stringify({
-                lon: gWorld1.getCenter().lng(), lat: gWorld1.getCenter().lat(),
-                rad: document.getElementById('radius2').value
-            }));
-        });
-    }
+    // if ($("#google_map_route").length > 0) {
+    //
+    //     var gWorldCords = new google.maps.LatLng(41.895562, -87.65097);
+    //     var gWorldOptions = {zoom: 15, center: gWorldCords, mapTypeId: google.maps.MapTypeId.ROADMAP};
+    //     var gWorld1 = new google.maps.Map(document.getElementById("google_map_route"), gWorldOptions);
+    //     initMap(gWorld1);
+    //     new AutocompleteDirectionsHandler(gWorld1);
+    //     var data = getSensors([gWorld1.getCenter().lng(), gWorld1.getCenter().lat(),
+    //         document.getElementById('radius2').value * 100]);
+    //     updateIcons(gWorld1, data);
+    //     gWorld1.addListener('dragend', function () {
+    //         var data = getSensors([gWorld1.getCenter().lng(), gWorld1.getCenter().lat(),
+    //             document.getElementById('radius2').value * 100]);
+    //         updateIcons(gWorld1, data);
+    //     });
+    // }
 
     var infoWindow;
 
@@ -199,7 +233,6 @@ $(function () {
             travelMode: 'DRIVING'
         }, function (response, status) {
             if (status === 'OK') {
-                getIcePath(response);
                 var array = [];
                 var point;
                 for (var i = 0; i < response.routes[0].legs[0].steps.length; i++) {
